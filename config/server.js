6034fcc4a7ca8cf7e97e826f4a4f4d34d6247bac
rogger <rogger.140998@gmail.com>
2021-08-31 22:10:13 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '85d4ada2e3cb66eb48704f91ac20a329'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d0d6f90-a1b3-4979-89c7-9f5ff986997d'
+};
