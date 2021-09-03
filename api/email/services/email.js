7147569f20d874b0375e976f0a0e390b967b713c
rogger <rogger.140998@gmail.com>
2021-09-03 18:06:21 -0500

    service send email

--- api/email/services/email.js
+++ api/email/services/email.js
@@ -2,6 +2,11 @@
 
 module.exports = {
     send: async (emails) => {
-
+        const emailOptions = {
+            to: emails,
+            subject: 'This is a test',
+            html: `<h1>Welcome!</h1><p>This is a test HTML email.</p>`,
+        }
+        await strapi.plugins['email'].services.email.send(emailOptions);
     }
 }
--- api/event/controllers/event.js
+++ api/event/controllers/event.js
@@ -9,17 +9,10 @@ module.exports = {
     changed: async (ctx) => {
         try{
             const { id } = ctx.params;
-            //const result = await strapi.query('event').find({ id });
             const result = await strapi.services.event.findOne({ id });
             const emails = result.students.map(e => e.email);
-            console.log(emails);
-            const emailOptions = {
-                to: emails,
-                subject: 'This is a test',
-                html: `<h1>Welcome!</h1><p>This is a test HTML email.</p>`,
-            }
-            await strapi.plugins['email'].services.email.send(emailOptions);
-            ctx.send("eehh?");
+            await strapi.services.email.send(emails);
+            ctx.send({ ok: true, message: 'Correos enviados.' });
         } catch(err) {
             strapi.log.error(`Error sending email`, err)
             ctx.send({ error: 'Error sending email' })
